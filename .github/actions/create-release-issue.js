cf281a55a7ffc775edcf5e3b321d66a7fd8d5432
andrey <talash10@mail.ru>
2023-07-16 01:03:15 +0300

    chore: fix create issue

--- .github/actions/create-release-issue.js
+++ .github/actions/create-release-issue.js
@@ -1,24 +1,27 @@
-"use strict";
-exports.__esModule = true;
-var node_fetch_1 = require("node-fetch");
-var core = require("@actions/core");
-var token = process.env.GITHUB_TOKEN;
-var repo = process.env.GITHUB_REPOSITORY;
-var actor = process.env.GITHUB_ACTOR;
-var version = process.env.GITHUB_REF_NAME;
-var headers = {
-    Authorization: "token ".concat(token),
-    Accept: 'application/vnd.github.v3+json'
+import fetch from 'node-fetch';
+import * as core from '@actions/core';
+
+const token = process.env.GITHUB_TOKEN;
+const repo = process.env.GITHUB_REPOSITORY;
+const actor = process.env.GITHUB_ACTOR;
+const version = process.env.GITHUB_REF_NAME;
+
+const headers = {
+    Authorization: `token ${token}`,
+    Accept: 'application/vnd.github.v3+json',
 };
-var date = new Date().toISOString().slice(0, 10);
-var body = "Release Information:\n\n- Author: ".concat(actor, "\n- Release Date: ").concat(date, "\n- Version: ").concat(version, "\n- Changelog: <Insert Changelog Here>");
-(0, node_fetch_1["default"])("https://api.github.com/repos/".concat(repo, "/issues"), {
+
+const date = new Date().toISOString().slice(0,10);
+const body = `Release Information:\n\n- Author: ${actor}\n- Release Date: ${date}\n- Version: ${version}\n- Changelog: <Insert Changelog Here>`;
+
+fetch(`https://api.github.com/repos/${repo}/issues`, {
     method: 'POST',
-    headers: headers,
+    headers,
     body: JSON.stringify({
         title: 'RELEASE',
-        body: body
-    })
+        body,
+    }),
 })
-    .then(function (response) { return response.json(); })
-    .then(function (data) { return console.log("Issue created with number ".concat(data.number)); })["catch"](function (error) { return core.setFailed("Action failed with error ".concat(error)); });
+    .then(response => response.json())
+    .then((data) => console.log(`Issue created with number ${data.number}`))
+    .catch(error => core.setFailed(`Action failed with error ${error}`));
