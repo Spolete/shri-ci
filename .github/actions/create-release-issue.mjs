7b740d07cd2b711681622f0126800eedfd6e4b43
andrey <talash10@mail.ru>
2023-07-16 01:59:32 +0300

    chore: fix create issue

--- .github/actions/create-release-issue.mjs
+++ .github/actions/create-release-issue.mjs
@@ -11,17 +11,40 @@ const headers = {
     Accept: 'application/vnd.github.v3+json',
 };
 
-const date = new Date().toISOString().slice(0,10);
-const body = `Release Information:\n\n- Author: ${actor}\n- Release Date: ${date}\n- Version: ${version}\n- Changelog: <Insert Changelog Here>`;
-
-fetch(`https://api.github.com/repos/${repo}/issues`, {
-    method: 'POST',
+// Fetch all issues
+fetch(`https://api.github.com/repos/${repo}/issues?state=all`, {
     headers,
-    body: JSON.stringify({
-        title: 'RELEASE',
-        body,
-    }),
 })
-    .then(response => response.json())
-    .then((data) => console.log(`Issue created with number ${data.number}`))
-    .catch(error => core.setFailed(`Action failed with error ${error}`));
+  .then(response => response.json())
+  .then((issues) => {
+      // Check if release issue already exists
+      const releaseIssue = issues.find(issue => issue.title === 'RELEASE' && issue.body.includes(`Version: ${version}`));
+      if (releaseIssue) {
+          // If it exists, add a comment
+          const commentBody = `New run of workflow.\n- Author: ${actor}`;
+          fetch(`https://api.github.com/repos/${repo}/issues/${releaseIssue.number}/comments`, {
+              method: 'POST',
+              headers,
+              body: JSON.stringify({
+                  body: commentBody,
+              }),
+          })
+            .then(response => response.json())
+            .then(() => console.log('Comment added to existing release issue.'))
+            .catch(error => core.setFailed(`Action failed with error ${error}`));
+      } else {
+          // If it doesn't exist, create a new issue
+          const issueBody = `Release Information:\n\n- Author: ${actor}\n- Release Date: ${new Date().toISOString().slice(0,10)}\n- Version: ${version}\n- Changelog: <Insert Changelog Here>`;
+          fetch(`https://api.github.com/repos/${repo}/issues`, {
+              method: 'POST',
+              headers,
+              body: JSON.stringify({
+                  title: 'RELEASE',
+                  body: issueBody,
+              }),
+          })
+            .then(response => response.json())
+            .then((data) => console.log(`Issue created with number ${data.number}`))
+            .catch(error => core.setFailed(`Action failed with error ${error}`));
+      }
+  });
