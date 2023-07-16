3923498d714c6c7ce3e75a6de9952ab691ea18eb
andrey <talash10@mail.ru>
2023-07-16 20:42:08 +0300

    chore: change deploy.yml

---
+++ .github/actions/add-comment-close.mjs
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch';
+import * as core from '@actions/core';
+
+const token = process.env.GITHUB_TOKEN;
+const repo = process.env.GITHUB_REPOSITORY;
+const version = process.env.GITHUB_REF_NAME;
+
+const headers = {
+  Authorization: `token ${token}`,
+  Accept: 'application/vnd.github.v3+json',
+};
+
+fetch(`https://api.github.com/repos/${repo}/issues?state=open`, {
+  headers,
+})
+  .then(response => response.json())
+  .then((issues) => {
+    // Check if release issue already exists
+    const releaseIssue = issues.find(issue => issue.title === 'RELEASE' && issue.body.includes(`Version: **${version}**`));
+    if (!releaseIssue) return;
+
+    const commentBody = "Deploy has been completed successfully.";
+
+    fetch(`https://api.github.com/repos/${repo}/issues/${releaseIssue.number}/comments`, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        body: commentBody,
+      }),
+    })
+      .then(response => response.json())
+      .then(() => console.log('Comment added about deploy close.'))
+      .catch(error => core.setFailed(`Action failed with error ${error}`));
+  });
+
