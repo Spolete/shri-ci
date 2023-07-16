d1b33b427cad9d3f90836a9ab15bb43607d98040
andrey <talash10@mail.ru>
2023-07-16 20:34:07 +0300

    chore: change release.yml

--- .github/actions/update-release-issue.mjs
+++ .github/actions/update-release-issue.mjs
@@ -5,6 +5,9 @@ const token = process.env.GITHUB_TOKEN;
 const repo = process.env.GITHUB_REPOSITORY;
 const version = process.env.GITHUB_REF_NAME;
 const testResult = process.env.TEST_RESULT;
+const runId = process.env.GITHUB_RUN_ID;
+
+const workflowURL = `https://github.com/${repo}/actions/runs/${runId}`;
 
 const headers = {
   Authorization: `token ${token}`,
@@ -22,8 +25,10 @@ fetch(`https://api.github.com/repos/${repo}/issues?state=open`, {
 
     const commentBody = testResult ? `
 Tests have failed.
+- [Workflow Run](${workflowURL})
 ` : `
 Tests have been completed successfully.
+- [Workflow Run](${workflowURL})
 `;
     fetch(`https://api.github.com/repos/${repo}/issues/${releaseIssue.number}/comments`, {
       method: 'POST',
