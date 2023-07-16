41ca0e834c516d014cd9b2439e6a4fb2572829a3
andrey <talash10@mail.ru>
2023-07-16 21:00:53 +0300

    chore: change cri

--- .github/actions/add-comment-close.mjs
+++ .github/actions/add-comment-close.mjs
@@ -3,33 +3,23 @@ import * as core from '@actions/core';
 
 const token = process.env.GITHUB_TOKEN;
 const repo = process.env.GITHUB_REPOSITORY;
-const version = process.env.GITHUB_REF_NAME;
+const issueNumber = process.env.GITHUB_ISSUE_NUMBER;
 
 const headers = {
   Authorization: `token ${token}`,
   Accept: 'application/vnd.github.v3+json',
 };
 
-fetch(`https://api.github.com/repos/${repo}/issues?state=open`, {
+const commentBody = "Deploy has been completed successfully.";
+
+fetch(`https://api.github.com/repos/${repo}/issues/${issueNumber}/comments`, {
+  method: 'POST',
   headers,
+  body: JSON.stringify({
+    body: commentBody,
+  }),
 })
   .then(response => response.json())
-  .then((issues) => {
-    // Check if release issue already exists
-    const releaseIssue = issues.find(issue => issue.title === 'RELEASE' && issue.body.includes(`Version: **${version}**`));
-    if (!releaseIssue) return;
-
-    const commentBody = "Deploy has been completed successfully.";
-
-    fetch(`https://api.github.com/repos/${repo}/issues/${releaseIssue.number}/comments`, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify({
-        body: commentBody,
-      }),
-    })
-      .then(response => response.json())
-      .then(() => console.log('Comment added about deploy close.'))
-      .catch(error => core.setFailed(`Action failed with error ${error}`));
-  });
+  .then(() => console.log('Comment added about deploy close.'))
+  .catch(error => core.setFailed(`Action failed with error ${error}`));
 
